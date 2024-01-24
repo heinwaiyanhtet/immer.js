93f43cb32b9d5078915048e3653c60d4b5f281ac
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-24 19:49:31 +0630

    added to node.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import {enableMapSet} from "immer"
+
+enableMapSet()
+
+// ...later
+import {produce} from "immer"
+
+const usersById_v1 = new Map([
+    ["michel", {name: "Michel Weststrate", country: "NL"}]
+])
+
+const usersById_v2 = produce(usersById_v1, draft => {
+    draft.get("michel").country = "UK"
+})
+
+
+console.log(usersById_v1.get("michel").country); // Output: NL
+console.log(usersById_v2.get("michel").country); // Output: UK
+
